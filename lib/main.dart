4552581ac940a7d828fc08943616059ea753a00d
Garrick Chou <guitarkid35@gmail.com>
2019-09-03 16:38:17 +0800

    fix: StlessWidget change StatfulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,12 +30,17 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: () {},
-              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed.');
+              },
               child: Image.asset('images/dice6.png'),
             ),
           ),
